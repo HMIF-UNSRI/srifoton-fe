bbccc9e10b2ef1c24d1f036a3831ebd38cfd51ac
Azie Melza Pratama <aziemp66@gmail.com>
2022-06-02 23:56:48 +0700

    Working With Register Page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -2,17 +2,16 @@ import React from "react";
 
 const Register = () => {
   return (
-    <div
-      style={{
-        background: `url(./src/Assets/background/ampera-bg.png)`,
-      }}
-      className="text-slate-600 w-full h-full"
-    >
+    <div className="text-white bg-slate-900 w-full h-screen pt-16">
       <div>
-        <h1 className="text-3xl font-japanese">Yahoo</h1>
+        <p className="text-[50px] text-center">HMIF UNSRI 2022</p>
+        <h1 className="font-japanese text-center text-[160px]">SRIFOTON</h1>
       </div>
-      <div>
-        <h2 className="text-xl">Register</h2>
+      <div className="bg-white text-black p-[40px] w-3/4 mx-auto">
+        <h2 className="text-5xl font-japanese text-center bg-gradient-to-r  from-red-primary to-red-secondary bg-clip-text text-transparent">
+          Register
+        </h2>
+        <p className="text-center">please fill in the data self completely </p>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,8 @@ module.exports = {
       colors: {
         "primary-dark-main": "rgba(0, 0, 0, 1)",
         "primary-dark-gradient": "rgba(0, 0, 0, 0)",
+        "red-primary": "rgba(196, 0, 24, 1)",
+        "red-secondary": "rgba(255, 97, 7, 1)",
       },
     },
   },
