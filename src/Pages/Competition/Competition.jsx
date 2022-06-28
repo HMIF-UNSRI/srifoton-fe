ea5b221a7b9f2094da35af6daedb021524373548
aziemp66 <aziemp66@gmail.com>
2022-06-29 00:19:57 +0700

    Adding Swiper JS

--- src/Pages/Competition/Competition.jsx
+++ src/Pages/Competition/Competition.jsx
@@ -3,26 +3,48 @@ import Background from "../../Components/Background/Background";
 
 import dashWhite from "../../Assets/Objects/dash-white.svg";
 
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+
 const Competition = () => {
   return (
     <Background>
-      <div className="relative my-8 w-3/4 md:w-1/2 lg:w-[40%] mx-auto flex flex-col">
-        <img src={dashWhite} alt="dash-top" className="absolute top-0 left-0" />
+      <div className="relative my-8 w-4/5 md:w-2/3 lg:w-1/2 mx-auto flex flex-col">
+        <img
+          src={dashWhite}
+          alt="dash-top"
+          className="absolute top-0 left-60"
+        />
         <h2 className="text-xl md:text-4xl lg:text-5xl  font-japanese text-center my-3 text-white">
           SRIFOTON 2022 COMPETITION
         </h2>
         <img
           src={dashWhite}
           alt="dash-bot"
-          className="absolute bottom-0 right-0"
+          className="absolute bottom-0 right-60"
         />
       </div>
-      <div>
-        <h2>Hemlo</h2>
-      </div>
-      <div>
-        <h3>Yahoo</h3>
-      </div>
+      <Swiper
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <p className="text-white">Hemlo</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p className="text-white">Hemlo</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p className="text-white">Hemlo</p>
+        </SwiperSlide>
+        <SwiperSlide>
+          <p className="text-white">Hemlo</p>
+        </SwiperSlide>
+      </Swiper>
     </Background>
   );
 };
