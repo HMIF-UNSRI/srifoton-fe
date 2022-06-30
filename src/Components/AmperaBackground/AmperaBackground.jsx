4e9d51bab0dffe1d21c4cc4e88d3fe227afe95c2
aziemp66 <aziemp66@gmail.com>
2022-06-30 11:03:04 +0700

    Fixing Background

--- src/Components/AmperaBackground/AmperaBackground.jsx
+++ src/Components/AmperaBackground/AmperaBackground.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-const AmperaBackground = ({ children }) => {
+const AmperaBackground = ({ children, className }) => {
   return (
-    <div className="bg-[rgb(34,34,34)] w-full min-h-screen  h-full bg-ampera bg-center py-1 bg-cover bg-no-repeat">
+    <div
+      className={`bg-[rgb(34,34,34)] w-full min-h-screen h-full bg-ampera bg-center py-1 bg-cover bg-no-repeat ${className}`}
+    >
       <div className="bg-[url('../public/background/blur.png')] text-white bg-contain bg-no-repeat md:bg-center">
         {children}
       </div>
--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Background = ({ children, className }) => {
   return (
     <div
-      className={`bg-[rgb(34,34,34)] w-full h-full bg-gradient bg-center py-1 bg-cover bg-repeat-y md:bg-no-repeat ${className}`}
+      className={`bg-[rgb(34,34,34)] min-h-screen w-full h-full bg-gradient bg-center py-1 bg-cover bg-repeat-y md:bg-no-repeat ${className}`}
     >
       <div className="bg-[url('../public/background/blur.png')] bg-contain bg-no-repeat md:bg-center">
         {children}
