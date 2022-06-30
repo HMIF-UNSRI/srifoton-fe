1e6bb862287403f8acc1e85ed11f572a0a4d0874
aziemp66 <aziemp66@gmail.com>
2022-06-30 22:04:24 +0700

    Completed team Page

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -3,12 +3,14 @@ import Background from "../../Components/Background/Background";
 
 import dashWhite from "../../Assets/Objects/dash-white.svg";
 
+import activeIcon from "../../Assets/Icons/active.svg";
+
 const Team = () => {
   return (
     <Background>
       <div className="relative my-8 w-3/4 md:w-1/2 lg:w-[40%] mx-auto flex flex-col">
         <img src={dashWhite} alt="dash-top" className="absolute top-0 left-0" />
-        <h2 className="text-3xl md:text-4xl lg:text-5xl  font-japanese text-center my-3 bg-gradient-to-r from-red-primary to-red-secondary bg-clip-text text-transparent">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl  font-japanese text-center my-3 text-white">
           Profil Tim
         </h2>
         <img
@@ -17,37 +19,176 @@ const Team = () => {
           className="absolute bottom-0 right-0"
         />
       </div>
-      <section className="bg-white text-black p-[40px] w-3/4 mx-auto mb-20 rounded-xl">
-        <form className="bg-gray-100 text-black p-[20px] mb-20 rounded-xl">
-          <div className="flex flex-col gap-2 md:gap-4 mb-10">
-            <label htmlFor="name" className="text-base md:text-xl ">
-              Team Name :
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              className="border text-center  focus:outline-red-primary border-red-secondary bg-gradient-to-r from-red-primary to-red-secondary bg-clip-text text-transparent w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
-              placeholder="Enter Your Tim Name Here"
-              disabled
-              value={"Melza Team"}
-            />
+      <form className="w-3/4 mx-auto bg-white ">
+        <section className="text-black p-[40px] w-full mx-auto rounded-xl">
+          <div className="bg-gray-100 text-black p-[20px] rounded-xl">
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="name" className="text-base md:text-xl ">
+                Team Name :
+              </label>
+              <div
+                type="text"
+                id="name"
+                name="name"
+                className="border text-center flex justify-center items-center focus:outline-red-primary border-red-secondary bg-gradient-to-br from-red-primary to-red-secondary bg-clip-text text-base md:text-2xl text-transparent w-full px-3 md:px-4 py-1 md:py-2 rounded-lg"
+                placeholder="Enter Your Tim Name Here"
+                disabled
+              >
+                <p>Melza team</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="name" className="text-base md:text-xl ">
+                Status :
+              </label>
+              <div
+                type="text"
+                id="name"
+                name="name"
+                disabled
+                className="border flex gap-2 justify-center items-center text-center focus:outline-red-primary border-red-secondary text-green-700 font-bold w-full px-3 md:px-4 py-1 md:py-2 text-base md:text-2xl rounded-lg"
+              >
+                <p>Terverifikasi</p>
+                <img src={activeIcon} alt="active" className="w-4 md:w-6" />
+              </div>
+            </div>
           </div>
-          <div>
-            <label htmlFor="name" className="text-base md:text-xl ">
-              Status :
-            </label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              disabled
-              value={"Active"}
-              className="border text-center focus:outline-red-primary border-red-secondary bg-gradient-to-r from-red-primary to-red-secondary bg-clip-text text-transparent w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
-            />
+        </section>
+        <section className="text-black p-[40px] w-full mx-auto rounded-xl">
+          <p className="focus:outline-red-primary border-red-secondary bg-gradient-to-r from-red-primary to-red-secondary bg-clip-text text-transparent mb-4 text-xl">
+            First Member data
+          </p>
+          <div className="bg-gray-100 text-black p-[20px] rounded-xl">
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="name" className="text-base md:text-xl ">
+                Name :
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="nim" className="text-base md:text-xl ">
+                NIM :
+              </label>
+              <input
+                type="text"
+                id="nim"
+                name="nim"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your NIM Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="email" className="text-base md:text-xl ">
+                Email :
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="university" className="text-base md:text-xl ">
+                University :
+              </label>
+              <input
+                type="text"
+                id="university"
+                name="university"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="phone" className="text-base md:text-xl ">
+                No Wa :
+              </label>
+              <input
+                type="text"
+                id="phone"
+                name="phone"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
           </div>
-        </form>
-      </section>
+        </section>
+        <section className="text-black p-[40px] w-full mx-auto rounded-xl">
+          <p className="focus:outline-red-primary border-red-secondary bg-gradient-to-r from-red-primary to-red-secondary bg-clip-text text-transparent mb-4 text-xl">
+            Second Member data
+          </p>
+          <div className="bg-gray-100 text-black p-[20px] rounded-xl">
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="name" className="text-base md:text-xl ">
+                Name :
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="nim" className="text-base md:text-xl ">
+                NIM :
+              </label>
+              <input
+                type="text"
+                id="nim"
+                name="nim"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your NIM Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="email" className="text-base md:text-xl ">
+                Email :
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="university" className="text-base md:text-xl ">
+                University :
+              </label>
+              <input
+                type="text"
+                id="university"
+                name="university"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
+            <div className="flex flex-col gap-2 md:gap-4 mb-10">
+              <label htmlFor="phone" className="text-base md:text-xl ">
+                No Wa :
+              </label>
+              <input
+                type="text"
+                id="phone"
+                name="phone"
+                className="border border-slate-400 w-full px-3 md:px-4 py-1 md:py-2 text-xs md:text-xl rounded-lg"
+                placeholder="Enter Your Name Here"
+              />
+            </div>
+          </div>
+        </section>
+      </form>
     </Background>
   );
 };
