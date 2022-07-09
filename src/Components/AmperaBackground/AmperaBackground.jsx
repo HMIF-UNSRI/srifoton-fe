80ee8df51a35d93f59cd2213013e3f6198ba317b
Irvan Malik <irvanmalik48@gmail.com>
2022-07-10 01:42:11 +0700

    Modify Backgrounds to include Navbar and Footer
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/Components/AmperaBackground/AmperaBackground.jsx
+++ src/Components/AmperaBackground/AmperaBackground.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const AmperaBackground = ({ children, className }) => {
   return (
     <div
-      className={`bg-[rgb(34,34,34)] w-full min-h-screen h-full bg-ampera bg-center py-1 bg-cover bg-no-repeat ${className}`}
+      className={`bg-[rgb(34,34,34)] w-full min-h-screen h-full bg-ampera bg-center bg-cover bg-no-repeat ${className}`}
     >
+      <Navbar />
       <div className="bg-[url('../public/background/blur.png')] min-h-screen text-white bg-contain bg-no-repeat md:bg-center">
         {children}
       </div>
+      <Footer />
     </div>
   );
 };
--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Background = ({ children, className }) => {
   return (
     <div
-      className={`bg-[rgb(34,34,34)] min-h-screen w-full h-full bg-gradient bg-center py-1 bg-cover bg-repeat-y md:bg-no-repeat ${className}`}
+      className={`bg-[rgb(34,34,34)] min-h-screen w-full h-full bg-gradient bg-center bg-cover bg-repeat-y md:bg-no-repeat ${className}`}
     >
+      <Navbar />
       <div className="bg-[url('../public/background/blur.png')] min-h-screen bg-contain bg-no-repeat md:bg-center">
         {children}
       </div>
+      <Footer />
     </div>
   );
 };
