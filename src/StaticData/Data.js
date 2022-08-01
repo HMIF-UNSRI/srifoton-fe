45e4ad463904fa315e877ea62de1ccac0e8d9a64
Irvan Malik <irvanmalik48@gmail.com>
2022-08-01 11:08:38 +0700

    Use single file for all static data
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/StaticData/Data.js
@@ -0,0 +1,98 @@
+import competitiveProgramming from "../Assets/CompetitionIcon/competitive-programming.png";
+import eSport from "../Assets/CompetitionIcon/e-sport.png";
+import uiuxDesign from "../Assets/CompetitionIcon/uiux-design.png";
+import webDevelopment from "../Assets/CompetitionIcon/web-development.png";
+
+export const competitions = [
+  {
+    title: "Competitive Programming",
+    image: competitiveProgramming,
+    desc: "Competitive Programming merupakan lomba untuk menyelesaikan permasalahan berupa soal dalam bentuk program dengan batas waktu tertentu. Lomba ini dikerjakan secara tim sebanyak x orang, lomba ini diadakan pada platform Hackerrank.",
+    link: "competitive-programming",
+    guidebook: "/guidebook.pdf",
+    contacts: [
+      {
+        name: "Contact Person 1",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      },
+      {
+        name: "Contact Person 2",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      }
+    ],
+  },
+  {
+    title: "Web Development",
+    image: webDevelopment,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum dolore totam a repudiandae nesciunt illo? Eius fugit architecto aliquid ea quo tenetur ratione culpa fuga sed! Fugit officiis earum doloribus.",
+    link: "web-development",
+    guidebook: "/guidebook.pdf",
+    contacts: [
+      {
+        name: "Contact Person 1",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      },
+      {
+        name: "Contact Person 2",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      }
+    ],
+  },
+  {
+    title: "UI/UX Design",
+    image: uiuxDesign,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum dolore totam a repudiandae nesciunt illo? Eius fugit architecto aliquid ea quo tenetur ratione culpa fuga sed! Fugit officiis earum doloribus.",
+    link: "ui-ux-design",
+    guidebook: "/guidebook.pdf",
+    contacts: [
+      {
+        name: "Contact Person 1",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      },
+      {
+        name: "Contact Person 2",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      }
+    ],
+  },
+  {
+    title: "E-Sport",
+    image: eSport,
+    desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum dolore totam a repudiandae nesciunt illo? Eius fugit architecto aliquid ea quo tenetur ratione culpa fuga sed! Fugit officiis earum doloribus.",
+    link: "esport",
+    guidebook: "/guidebook.pdf",
+    contacts: [
+      {
+        name: "Contact Person 1",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      },
+      {
+        name: "Contact Person 2",
+        wa: "+62 821-2345-6789",
+        line: "@placeholdervalue"
+      }
+    ],
+  },
+];
+  
+export const sponsors = [
+  {
+    src: "/sponsors/sp4.png",
+  },
+  {
+    src: "/sponsors/sp5.png",
+  },
+  {
+    src: "/sponsors/sp6.png",
+  },
+  {
+    src: "/sponsors/sp7.png",
+  },
+];
